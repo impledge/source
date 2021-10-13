6e99170136e903b0d9b25b18b50654960a9d241d
cloudguruab <ap.brown011@gmail.com>
2021-10-12 23:12:47 -0400

    setup fetch api to next route api/contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,22 +1,42 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import { MailIcon } from '@heroicons/react/solid'
 
+/**
+ * @name ContactForm
+ * @description
+ * @returns
+ * 
+ * TODO:
+ * https://dev.to/markdrew53/integrating-sendgrid-with-next-js-4f5m
+ * Update fetch with new sengrid conf 
+ * edit styles for email component -> .container
+ */
 
-export default function ContactForm() {
+
+export default function ContactForm(props) {
     const [email, setEmail] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        setEmail((email) => {
-            email == event.target.value;
+        setEmail((value) => {
+            value == event.target.value;
         })
 
-        // fetch() ... set this to call api route
+        fetch("api/contact", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email: email
+            })
+        })
+        .then(res => res.json())
+        .then((myJson) => {console.log(myJson)})
+        .catch((err) => {console.log(err)});
     }
     
     return (
-        <div>
+        <div className="container">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                 Email
             </label>
@@ -28,8 +48,8 @@ export default function ContactForm() {
                 onChange={handleSubmit}
                 type="text"
                 name="email"
-                id={email}
-                value={value}
+                id="email"
+                value={email}
                 className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
                 placeholder="you@example.com"
                 />
