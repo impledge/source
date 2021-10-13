199616fee6dcfd2b98a34fecc84c287306200eee
cloudguruab <ap.brown011@gmail.com>
2021-10-12 22:01:38 -0400

    business logic

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import "tailwindcss/tailwind.css";
+
+export default function Home() {
+	
+	return (
+		<div>
+			<h1>Source Projects consulting</h1>
+			{/* meta data -> seo */}
+			{/* header */}
+			{/* body components */}
+			{/* footer */}
+		</div>
+	);
+}
