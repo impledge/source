c32175a298ac60744b0aa38aef3d03d3efd3845d
cloudguruab <ap.brown011@gmail.com>
2021-10-12 23:15:28 -0400

    index script foor building landing page foundation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import "tailwindcss/tailwind.css";
+import SafeAreaContainer from '../components/Container';
 
 export default function Home() {
-	
+
 	return (
 		<div>
-			<h1>Source Projects consulting</h1>
+			<SafeAreaContainer>
+				<h1>Welcome to useSource</h1>
+			</SafeAreaContainer>
 			{/* meta data -> seo */}
 			{/* header */}
 			{/* body components */}
