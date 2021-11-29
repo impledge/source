2185477c1101bb7b83556ecfec7ed91398ba22ae
cloudguruab <ap.brown011@gmail.com>
2021-11-28 21:09:56 -0500

    changed .inc to llc

--- components/Footer.js
+++ components/Footer.js
@@ -55,7 +55,7 @@ export default function Example() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base text-gray-400">
-            &copy; 2020 The Source Projcet @useSource, Inc. All rights reserved.
+            &copy; 2020 The Source Projcet @useSource, LLC. All rights reserved.
           </p>
         </div>
       </div>
