a7a94a250e2eeede6c38b46a121f3f87ae30d817
cloudguruab <ap.brown011@gmail.com>
2021-12-16 11:28:21 -0500

    config docs page

---
+++ pages/docs.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+export default function Docs() {
+    //logic for docs 
+    return (
+        <>
+        <Header />
+        <h1>This is our docs site</h1>
+        </>
+    );
+}
